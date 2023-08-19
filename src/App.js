b4542476dcd8a5b44518a81f7e33f636d14efa95
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-19 10:34:35 +0530

    Dynamic functionality added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { useEffect } from 'react'
 import SearchIcon from './search.svg'
@@ -14,11 +14,13 @@ const movie1 = {
 }
 
 const App = () => {
-    
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState();
     const searchMovie = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json();
-        console.log(data.Search);
+        setMovies(data.Search);
     }
     
     useEffect(()=>{
@@ -28,12 +30,30 @@ const App = () => {
     <div className='app'>
         <h1>Moviecorn</h1>
         <div className='search'>
-            <input placeholder='Search' value='Superman'/>
-            <img src={SearchIcon} alt="search" onClick={() => {}} />
+            <input 
+                placeholder='Search' 
+                value={searchTerm} 
+                onChange={(e) => {setSearchTerm(e.target.value)}}
+            />
+            <img 
+                src={SearchIcon} 
+                alt="search" 
+                onClick={() => {searchMovie(searchTerm)}} />
         </div>
-        <div className='container'>
-            <MovieCard movie1={movie1}/>
-        </div>
+
+        {
+            movies?.length > 0 ? (
+                <div className='container'>
+                    {movies.map((movie) =>(
+                        <MovieCard movie={movie}/>
+                    ))}
+                </div>
+            ) : (
+                <div className='empty'>
+                    <h2>No movies</h2>
+                </div>
+            )
+        }
     </div>
   )
 }
